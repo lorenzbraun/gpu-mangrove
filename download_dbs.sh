2786cb743641afa486a30dd92b098d3c9f176e3d
Lorenz Braun <lorenz.braun@ziti.uni-heidelberg.de>
2020-12-07 14:48:23 +0100

    updating download link for datasets

--- download_dbs.sh
+++ download_dbs.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f data.tar.gz ]; then
   echo downloading data.tar.gz
-  wget -O data.tar.gz https://heibox.uni-heidelberg.de/f/c0991490e0d4498689ef/?dl=1
+  wget -O data.tar.gz https://heibox.uni-heidelberg.de/f/938997f66aef46fc9188/?dl=1
 fi
 
 if [ ! -d data ]; then
